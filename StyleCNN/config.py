140bd70ad4ded8798eec5e947586ae0b8626fe45
YYujie-Kampf <113015061+YYujie-Kampf@users.noreply.github.com>
2023-12-10 18:24:26 -0800

    Add files for StyleCNN

---
+++ StyleCNN/config.py
@@ -0,0 +1,34 @@
+import sys, os
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '../dataset/')
+
+
+CLASS_LIST = ["axel", "korky", "polacco", "mbrown", "dmckee", "khenkes", "rcurto", "tross", "serap",  "scart", "photo"]
+
+CLASS_COUNT = len(CLASS_LIST)
+BATCH_SIZE = 32
+EPOCH_COUNT = 400
+SAVE_PERIOD_IN_EPOCHS = 5
+LOG_STEP = 100
+LEARNING_RATE = 2.5e-4
+NUM_WORKERS = 32
+
+USE_PATCH = 1
+PATCH_SIZE = 150
+RESIZE = 224
+
+# model save dir
+PYTORCH_MODELS = '/content/drive/Shareddrives/CS236_Drawing_Wizard/checkpoints/Style/'
+
+LOAD_TRAINED_MODEL = 0 # if you want to load a model during train change this to 1.
+
+# train dataset
+train_dir_path = '../dataset_style/train'
+train_path = train_dir_path + "/"
+
+# test dataset
+test_dir_path = '/content/drive/Shareddrives/CS236_Drawing_Wizard/results/dataset_style_ganilla/test'
+test_path = test_dir_path + '/'
+
+# test_model name
+MODEL_NAME = "/content/drive/Shareddrives/CS236_Drawing_Wizard/checkpoints/Style/art_model-159.pkl"
+
